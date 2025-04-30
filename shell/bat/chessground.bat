d8d161fce137026d398a6a57bb9bc43ee0ff5740
JakimPL <jakimpl@gmail.com>
2025-04-30 23:20:50 +0200

    Fixed: installation script

--- shell/bat/chessground.bat
+++ shell/bat/chessground.bat
@@ -1,5 +1,5 @@
 @echo off
-npm install --save chessground
+call npm install --save chessground
 
 copy node_modules\chessground\assets\chessground.base.css static\css\import\
 copy node_modules\chessground\assets\chessground.brown.css static\css\import\
